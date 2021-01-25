51a4d40308d75ab4572eefc30ee19ab29daf2afe
Alejandro Vivas <alejandrojv17@gmail.com>
2021-01-25 11:31:41 -0300

    Added context api, removed redyx store

---
+++ pages/profile.js
@@ -0,0 +1,12 @@
+import Layout from '../components/global/Layout'
+
+const Index = () => {
+
+    return ( 
+        <Layout location="profile">
+            profile
+        </Layout>
+     );
+}
+
+export default Index;
